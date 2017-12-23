d5dd9d168c4c38c563a384137567e9fb931ac844
Ltsinh <linhtsinh@gmail.com>
2017-12-23 00:27:34 +0000

    week2

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,12 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
   validates :name, presence: true, length: { maximum: 50, minimum: 3 }
   validates :address, presence: true
+  
   validates :description, presence: true
 end
